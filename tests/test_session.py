8dabc6ad0591ee65d2b92a396d9fc880484f318b
Ben Chen <bn@benbenbang.io>
2022-10-30 22:45:48 +0100

    tests(utests): update cache api and utests

--- tests/test_session.py
+++ tests/test_session.py
@@ -1,5 +1,7 @@
 # standard library
+import pickle
 import tempfile
+from datetime import datetime, timedelta
 
 # pypi/conda library
 import flask
@@ -14,23 +16,18 @@ def test_null_session():
     app = flask.Flask(__name__)
     Session(app)
 
-    def expect_exception(f, *args, **kwargs):
-        try:
-            f(*args, **kwargs)
-        except RuntimeError as e:
-            assert e.args and "session is unavailable" in e.args[0]
-        else:
-            assert False, "expected exception"
-
     with app.test_request_context():
         assert flask.session.get("missing_key") is None
 
-        expect_exception(flask.session.__setitem__, "foo", 42)
-        expect_exception(flask.session.pop, "foo")
+        with pytest.raises(RuntimeError):
+            flask.session["foo"] = 42
+
+        with pytest.raises(RuntimeError):
+            flask.session.pop("foo")
 
 
 @pytest.mark.unittests
-def test_redis_session():
+def test_redis_session(mocker):
     app = flask.Flask(__name__)
     app.config["SESSION_TYPE"] = "redis"
     Session(app)
@@ -42,21 +39,25 @@ def test_redis_session():
 
     @app.route("/get")
     def get():
-        return flask.session["value"]
+        return flask.jsonify(value=flask.session["value"])
 
     @app.route("/delete", methods=["POST"])
     def delete():
         del flask.session["value"]
         return "value deleted"
 
+    mocker.patch("redis.StrictRedis.setex").return_value = set
+    mocker.patch("redis.StrictRedis.get").return_value = pickle.dumps({"value": "42"})
+    mocker.patch("redis.StrictRedis.delete").return_value = delete
+
     c = app.test_client()
-    assert c.post("/set", data={"value": "42"}).data, b"value set"
-    assert c.get("/get").data == b"42"
-    c.post("/delete")
+    assert c.post("/set", data={"value": "42"}).data == b"value set"
+    assert c.get("/get").json["value"] == "42"
+    assert c.post("/delete").data == b"value deleted"
 
 
 @pytest.mark.unittests
-def test_memcached_session():
+def test_memcached_session(mocker):
     app = flask.Flask(__name__)
     app.config["SESSION_TYPE"] = "memcached"
     Session(app)
@@ -75,10 +76,14 @@ def test_memcached_session():
         del flask.session["value"]
         return "value deleted"
 
+    mocker.patch("pylibmc.client.Client.set").return_value = set
+    mocker.patch("pylibmc.client.Client.get").return_value = pickle.dumps({"value": "42"})
+    mocker.patch("pylibmc.client.Client.delete").return_value = delete
+
     c = app.test_client()
-    assert c.post("/set", data={"value": "42"}).data, b"value set"
+    assert c.post("/set", data={"value": "42"}).data == b"value set"
     assert c.get("/get").data == b"42"
-    c.post("/delete")
+    assert c.post("/delete").data == b"value deleted"
 
 
 @pytest.mark.unittests
@@ -103,13 +108,13 @@ def test_filesystem_session():
         return "value deleted"
 
     c = app.test_client()
-    assert c.post("/set", data={"value": "42"}).data, b"value set"
+    assert c.post("/set", data={"value": "42"}).data == b"value set"
     assert c.get("/get").data == b"42"
     c.post("/delete")
 
 
 @pytest.mark.unittests
-def test_mongodb_session():
+def test_mongodb_session(mocker):
     app = flask.Flask(__name__)
     app.testing = True
     app.config["SESSION_TYPE"] = "mongodb"
@@ -122,26 +127,39 @@ def test_mongodb_session():
 
     @app.route("/get")
     def get():
-        return flask.session["value"]
+        return flask.jsonify(value=flask.session["value"])
 
     @app.route("/delete", methods=["POST"])
     def delete():
         del flask.session["value"]
         return "value deleted"
 
+    mocker.patch("pymongo.collection.Collection.update_one").return_value = set
+    mocker.patch("pymongo.collection.Collection.find_one").return_value = {
+        "val": pickle.dumps({"value": "42"}),
+        "expiration": datetime.utcnow() + timedelta(days=1),
+    }
+    mocker.patch("pymongo.collection.Collection.delete_one").return_value = delete
+
     c = app.test_client()
     assert c.post("/set", data={"value": "42"}).data == b"value set"
-    assert c.get("/get").data == b"42"
-    c.post("/delete")
+    assert c.get("/get").json == {"value": "42"}
+    assert c.post("/delete").data == b"value deleted"
 
 
 @pytest.mark.unittests
 def test_flasksqlalchemy_session():
+
     app = flask.Flask(__name__)
     app.debug = True
     app.config["SESSION_TYPE"] = "sqlalchemy"
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sessions.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+
     Session(app)
+
+    # create the database and the db-table
+    app.session_interface.db.create_all()
 
     @app.route("/set", methods=["POST"])
     def set():
@@ -160,19 +178,24 @@ def test_flasksqlalchemy_session():
     c = app.test_client()
     assert c.post("/set", data={"value": "42"}).data == (b"value set")
     assert c.get("/get").data == b"42"
-    c.post("/delete")
+    assert c.post("/delete").data == b"value deleted"
 
 
 @pytest.mark.unittests
 def test_flasksqlalchemy_session_with_signer():
+
     app = flask.Flask(__name__)
     app.debug = True
     app.secret_key = "test_secret_key"
     app.config["SESSION_TYPE"] = "sqlalchemy"
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sessions.db"
     app.config["SESSION_USE_SIGNER"] = True
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
     Session(app)
+
+    # create the database and the db-table
+    app.session_interface.db.create_all()
 
     @app.route("/set", methods=["POST"])
     def set():
@@ -191,11 +214,11 @@ def test_flasksqlalchemy_session_with_signer():
     c = app.test_client()
     assert c.post("/set", data={"value": "42"}).data == b"value set"
     assert c.get("/get").data == b"42"
-    c.post("/delete")
+    assert c.post("/delete").data == b"value deleted"
 
 
 @pytest.mark.unittests
-def test_session_use_signer():
+def test_session_use_signer(mocker):
     app = flask.Flask(__name__)
     app.secret_key = "test_secret_key"
     app.config["SESSION_TYPE"] = "redis"
@@ -211,6 +234,16 @@ def test_session_use_signer():
     def get():
         return flask.session["value"]
 
+    @app.route("/delete", methods=["POST"])
+    def delete():
+        del flask.session["value"]
+        return "value deleted"
+
+    mocker.patch("redis.StrictRedis.setex").return_value = set
+    mocker.patch("redis.StrictRedis.get").return_value = pickle.dumps({"value": "42"})
+    mocker.patch("redis.StrictRedis.delete").return_value = delete
+
     c = app.test_client()
-    assert c.post("/set", data={"value": "42"}).data, b"value set"
+    assert c.post("/set", data={"value": "42"}).data == b"value set"
     assert c.get("/get").data == b"42"
+    assert c.post("/delete").data == b"value deleted"
